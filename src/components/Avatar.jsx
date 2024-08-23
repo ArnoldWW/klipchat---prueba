ddb89f0c7580ae1cbea4a4c118385b75550a9f80
arnold rodriguez <arnolddavidrd@gmail.com>
2024-08-23 10:05:28 -0500

    bg color gris

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,7 +1,8 @@
 export default function Avatar({ image = "/user.jpg" }) {
   return (
-    <div className="rounded-full bg-red-500 self-start w-10 h-10 overflow-hidden">
+    <div className="rounded-full bg-neutral-700 self-start w-10 h-10 overflow-hidden">
       <img src={image} className="object-cover" alt="user" />
     </div>
   );
 }
+asdaaaaaaaaaaaa;
