4218f983330b7c50f42801b80566b079bd2a4d06
arnold rodriguez <arnolddavidrd@gmail.com>
2024-08-26 13:46:42 -0500

    optimizacion de imagenes

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,7 +1,7 @@
 export default function Avatar({ image = "/user.jpg" }) {
   return (
     <div className="rounded-full bg-neutral-700 self-start w-10 h-10 overflow-hidden">
-      <img src={image} className="object-cover" alt="user" />
+      <img src={image} className="object-cover h-full w-full" alt="user" />
     </div>
   );
 }
